24ecef99bf4258b84017f63b84548b8e17764040
Becka McNally <beckamcnally@gmail.com>
2023-02-06 12:01:10 -0800

    location routes

--- routes/apis/location-routes.js
+++ routes/apis/location-routes.js
@@ -1,4 +1,51 @@
 const router = require("express").Router();
 const {Location, Traveller, Trips} = require("../../models");
 
+router.get('/', async (req, res) => {
+  try {
+    const locationData = await Location.findAll({
+      include: [{model: location}]
+    });
+    res.status(200).json(locationData);
+  } catch (err) {res.status(500).json(err)}
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const locationData = await Location.findByPk(req.params.id, {
+      include: [{model: Location}, {model: Trips}],
+      where: {
+        id: req.params.id
+      }
+      
+    });
+    if (!locationData){
+      res.status(404).json({ message: 'Not found'});
+      return;
+    }
+    res.status(200).json(locationData);
+  } catch (err) {res.status(500).json(err)}
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const locationData = await Location.create({
+      location_name: req.body
+  });
+    res.status(200).json(locationData);
+  } catch (err) {res.status(500).json(err)}
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const locationData = await Location.destroy({
+     where: {
+      id: req.params.id
+     }
+  });
+    res.status(200).json(locationData);
+  } catch (err) {res.status(500).json(err)}
+});
+
+
 module.exports = router;
--- routes/apis/traveller-routes.js
+++ routes/apis/traveller-routes.js
@@ -1,4 +1,46 @@
 const router = require("express").Router();
 const {Location, Traveller, Trips} = require("../../models");
 
+router.get('/', async (req, res) => {
+  try {
+    const travellerData = await Location.findAll({
+      include: [{model: traveller}]
+    });
+    res.status(200).json(traveller);
+  } catch (err) {res.status(500).json(err)}
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const travellerData = await Location.findByPk(req.params.id, {
+      include: [{model: traveller}]
+    });
+    if (!travellerData){
+      res.status(404).json({ message: 'Not found'});
+      return;
+    }
+    res.status(200).json(travellerData);
+  } catch (err) {res.status(500).json(err)}
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const travellerData = await Location.create({
+      location_name: req.body
+  });
+    res.status(200).json(travellerData);
+  } catch (err) {res.status(500).json(err)}
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const travellerData = await Location.destroy({
+     where: {
+      id: req.params.id
+     }
+  });
+    res.status(200).json(travellerData);
+  } catch (err) {res.status(500).json(err)}
+});
+
 module.exports = router;
